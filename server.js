72db331148f729e182209035d33019e81292baf8
syukronarie <syukronarie@gmail.com>
2021-02-14 16:55:17 +0700

    add server

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const http = require("http");
 const server = http.createServer((req, res) => {
 	// console.log("request made");
 	console.log(req.url, req.method);
+
+	// set header content type
+	// res.setHeader("Content-Type", "text/plain");
+
+	// res.write("hello from server");
+	// res.end();
+
+	res.setHeader("Content-Type", "text/html");
+
+	res.write("hello from server");
+	res.end();
 });
 
 server.listen(3000, "localhost", () => {
